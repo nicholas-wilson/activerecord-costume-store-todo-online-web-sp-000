e52ac9c53422fbe5f550c13d906dbedb54fb771b
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 00:26:43 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,14 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
-
+  def change
+    create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.string :theme
+      h.float :price
+      h.boolean :family_friendly
+      h.string :opening_date
+      h.string :closing_date
+      h.string :description
+  end
 end
