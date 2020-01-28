c70d36c0a93da3d32846810fba66911d2469b06c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 00:00:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,5 +8,6 @@
 # In this change method, create columns with the correct names and
 # value types according to the spec
 class CreateCostumes < ActiveRecord:Migration[4.2]
-
+  create_table :costumes do |c|
+    c.name :string
 end
